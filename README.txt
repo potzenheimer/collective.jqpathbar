aa3cafd01469d5bbc4db4771488011db70213101
Christoph Boehner <cb@vorwaerts-werbung.de>
2011-06-20 20:41:56 +0200

    Initital package layout created with zopeskel

---
+++ README.txt
@@ -0,0 +1,9 @@
+Introduction
+============
+
+This package replaces the Plone `portal_breadcrumbs` with a jquery.breadcrumbs.js enhanced pathbar
+that auto collapses and therefore doesn't inhabit as much space as the default pathbar.
+
+It utilizes z3c.jbot to provide a custom template that uses an unordered list for the breadcrumb trail.
+
+
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0a1'
+
+setup(name='collective.jqpathbar',
+      version=version,
+      description="Replace plone breadcrumbs with a jquery.breadCrumbs.js enabled pathbar.",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Vorwaerts Werbung GbR',
+      author_email='hallo@vorwaerts-werbung.de',
+      url='http://dist.vorwaerts-werbung.de',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["ZopeSkel"],
+      )
